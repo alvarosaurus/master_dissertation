1ad46717c027dc3fe04c64581de42c21156b3e1c
Alvaro Ortiz-Troncoso <alvaro@smultron.org>
2017-12-15 19:09:50 +0100

    Added tables for Chapter 2

--- docs/Chapters/auto/Chapter2.el
+++ docs/Chapters/auto/Chapter2.el
@@ -2,6 +2,10 @@
  "Chapter2"
  (lambda ()
    (LaTeX-add-labels
-    "fig:objectives"))
+    "fig:objectives"
+    "table:objective1"
+    "table:objective2"
+    "table:objective3"
+    "table:objective4"))
  :latex)
 
